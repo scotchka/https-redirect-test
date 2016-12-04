ed49deedd789f3cceb17a7f8e47eeb463b4af805
Henry Chen <tahafut@gmail.com>
2016-12-03 17:47:11 -0800

    add absolute redirect

--- server.py
+++ server.py
@@ -8,12 +8,19 @@ app = Flask(__name__)
 def index():
 	return render_template_string("""
 		<a href="/">home</a>
+		<br>
 		<a href="/redirect">redirect to home</a>
+		<br>
+		<a href="/absolute-redirect">absolute redirect to home</a>
 		""")
 
 @app.route('/redirect')
 def other():
 	return redirect('/')
+
+@app.route('/absolute-redirect')
+def another():
+	return redirect('https://https-redirect-test.herokuapp.com')
 
 
 if __name__ == '__main__':
